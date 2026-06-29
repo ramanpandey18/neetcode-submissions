ff2e5a03899f8832c146998a9de9eb36beeadb9a
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-06-29 13:47:19 +0530

    Add: min-cost-climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = {}
+        n = len(cost)
+        def get_min_cost(i: int) -> int:
+            if i >= n:
+                return 0
+            if i in memo:
+                return memo[i]
+            take_one = cost[i] + get_min_cost(i + 1)
+            take_two = cost[i] + get_min_cost(i + 2)
+            memo[i] = min(take_one, take_two)
+            return memo[i]
+        return min(get_min_cost(0), get_min_cost(1))
+
+
+        def get_min_cost(i: int) -> int:
+            if i >= n:
+                return 0
+            if i in memo:
+                return memo[i]
+            take_one = cost[i] + get_min_cost(i + 1)
+            take_two = cost[i] + get_min_cost(i + 2)
+
+            memo[i] = min(take_one, take_two)
+            return memo[i]
+        return min(get_min_cost(0), get_min_cost(1))
+
+        
