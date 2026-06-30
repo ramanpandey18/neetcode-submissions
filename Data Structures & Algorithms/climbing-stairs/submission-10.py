a3c7721bd1a27b2d35d2bbd7a5ff17e787cb8cce
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-06-30 18:52:00 +0530

    Add: climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/climbing-stairs/submission-10.py
@@ -0,0 +1,38 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def helper(steps_left: int) -> int:
+            if steps_left == 0:
+                return 1
+            if steps_left < 0:
+                return 0
+            if steps_left in memo:
+                return memo[steps_left]
+            memo[steps_left] = helper(steps_left - 1) + helper(steps_left - 2)
+            return memo[steps_left]
+        return helper(n)
+
+        # memo = {}
+        # def helper(steps_left: int) -> int:
+        #     if steps_left == 0:
+        #         return 1
+        #     if steps_left < 0:
+        #         return 0
+        #     if steps_left in memo:
+        #         return memo[steps_left]
+        #     memo[steps_left] = helper(steps_left - 1) + helper(steps_left - 2)
+        #     return memo[steps_left]
+        # return helper(n)
+
+        
+    # def climbStairs(n: int) -> int:
+        # Base Cases: 
+        # If 0 steps left, we found 1 valid way (standing at the top).
+        # If we overshoot (negative steps), this path is invalid (0 ways).
+        # if n == 0: return 1
+        # if n < 0: return 0
+        
+        # Recursive Step: Sum of taking 1 step + taking 2 steps
+        # return climbStairs(n - 1) + climbStairs(n - 2)
+
+    # print(climbStairs(4))
