8abf7f1b9d09f5a95dda8650b2e4d40182b4f808
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-07-29 15:15:58 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (seen.containsKey(nums[i])) {
+                return true;
+            }
+            seen.put(nums[i], i);
+        }
+        return false;
+    }
+}
+
