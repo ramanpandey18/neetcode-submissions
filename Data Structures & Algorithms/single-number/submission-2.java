49c761a73028e4af8844f7c860fbd83e91619246
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-07-29 19:10:53 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num: nums){
+            result ^= num;
+        }
+        return result;
+
+
+        // result = 0
+        // for num in nums:
+        //     result ^= num
+        // return result
+
+    }
+}
