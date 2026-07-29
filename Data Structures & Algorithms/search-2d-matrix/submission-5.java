6ab060c3daed07c4091787415757c5c71dcaf11d
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-07-29 20:57:12 +0530

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = (m * n) - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int val = matrix[mid / n][mid % n];
+            if (val == target) {
+                return true;
+            } else if (val < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+        // m = len(matrix)
+        // n = len(matrix[0])
+        // low = 0
+        // high = m * n - 1
+        // while low <= high:
+        //     mid = (low + high) // 2
+        //     val = matrix[mid // n][mid % n]
+        //     if val == target:
+        //         return True
+        //     elif val < target:
+        //         low = mid + 1
+        //     else:
+        //         high = mid - 1
+        // return False
