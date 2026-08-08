ea4cf5cafb52d7ad6eb739b06c18e376ec278fb9
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-08-09 00:39:32 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    # def rob(self, nums: List[int]) -> int:
+    #     return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+    
+    # def helper(self, nums):
+    #     rob1 = rob2 = 0
+    #     for num in nums:
+    #         tempRob = max(rob1 + num, rob2)
+    #         rob1 = rob2
+    #         rob2 = tempRob
+    #     return rob2
+
+
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(
+            self.helper(nums[:-1]),
+            self.helper(nums[1:])
+        )
+
+    def helper(self, nums):
+        rob1 = 0
+        rob2 = 0
+
+        for num in nums:
+            current = max(
+                rob2,
+                rob1 + num
+            )
+
+            rob1 = rob2
+            rob2 = current
+
+        return rob2
