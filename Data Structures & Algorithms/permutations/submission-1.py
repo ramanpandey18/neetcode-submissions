02df356e8e9c660bd40cfae1a574a713c0777125
Raman Pandey <134055334+ramanpandey18@users.noreply.github.com>
2026-09-26 11:19:24 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # [1,2,3]
+        # [2,1,3] [3,2,1]
+        # [2,3,1] [3,1,2]
+
+        res = []
+        n = len(nums)
+        visited = [False] * n
+
+        def backtrack(current):
+            if len(current) == n:
+                res.append(list(current))
+                return
+            
+            for i in range(n):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                current.append(nums[i])
+                backtrack(current)
+                visited[i] = False
+                current.pop()
+        
+        backtrack([])
+
+        return res
+        
